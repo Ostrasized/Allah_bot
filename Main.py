e523f98b8ad9eaa44ebef3f9c6c1c4f13c633026
Ostrazied <69784450+Ostrasized@users.noreply.github.com>
2021-02-11 20:36:54 -0800

    Added Quran API

--- Main.py
+++ Main.py
@@ -1,4 +1,10 @@
 
+import json
+
+import requests
+
+import random
+
 import os
 
 from dotenv import load_dotenv
@@ -10,7 +16,12 @@ class Allah(discord.Client):
         print('logged on as ' + str(self.user))
     async def on_message(self, message):
         if(message.content == 'Oh Allah!'):
-            await message.channel.send('Yes, my child') # WIll change to Qur'An
+            Verse = random.randint(1,6236)
+            response = requests.get('http://api.alquran.cloud/v1/ayah/'+ str(Verse)+ '/en.asad')
+            thingy = response.json()
+           
+            await message.channel.send( thingy['data']['text'])
+
 load_dotenv('.env')
 client = Allah()
 client.run(os.getenv('Allah_token')) 
